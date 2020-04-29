30d1455612e318e3b7c6cef9a46e9a348b417fb6
zevit0r <z3.vit07@gmail.com>
2020-04-29 00:15:20 -0300

    user registration and user session

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,53 @@
+import User from "../models/User";
+
+class UserController {
+    async store(req, res) {
+        const userExists = await User.findOne({
+            where: { email: req.body.email },
+        });
+
+        if (userExists) {
+            return res.status(400).json({ error: "user already exists" });
+        }
+
+        const { id, name, email, provide } = await User.create(req.body);
+
+        return res.json({
+            id,
+            name,
+            email,
+            provide,
+        });
+    }
+
+    async update(req, res) {
+        const { email, oldPassword } = req.body;
+
+        const user = await User.findOne(req.userId);
+
+        if (email !== user.email) {
+            const userExists = await User.findOne({
+                where: { email },
+            });
+
+            if (userExists) {
+                return res.status(400).json({ error: "user already exists" });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(password))) {
+            return res.status(401).json({ error: "Password does not match" });
+        }
+
+        const { id, name, provide } = await user.update(req.body);
+
+        return res.json({
+            id,
+            name,
+            email,
+            provide,
+        });
+    }
+}
+
+export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: "71603dd5363da38dde073dcaa09d7ad2",
+    expiresIn: "7d",
+};
